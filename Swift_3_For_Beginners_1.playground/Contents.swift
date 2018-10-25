c79b38ebc96077a849a366b4c796c49f565f630d
freedom-diy <170829561@qq.com>
2018-10-26 03:58:33 +0800

    add answers 5

--- Swift_3_For_Beginners_1.playground/Contents.swift
+++ Swift_3_For_Beginners_1.playground/Contents.swift
@@ -1,14 +1,46 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
 //Submission correctly answers Exercise 1.
 
-var petsAge = 12
-var myMiddleInitial: Character = "A"
-var centimetersOfRainfall: Float = 5.5
-var gallonsRemainingInTank: Double = 3.14159265359
-var ageOfSpecimenInWeeks: Int = 4
-var birthplace: String  = "nudibranch"
-var rainingOutside: Bool  = true
+//var petsAge = 12
+//var myMiddleInitial:Character = "A"
+//var centimetersOfRainfall:Float = 5.5
+//var gallonsRemainingInTank:Double = 3.14
+//var ageOfSpecimenInWeeks:Int = 4
+//var birthplace:String  = "nudibranch"
+//var rainingOutside: Bool  = true
 
+
+//Submission correctly answers Exercise 2.
+
+//var favoriteLanguage:String = "Swift"
+//var gallonsRemainingInTank:String = "2"
+//var ageOfSpecimenInWeeks:Double = 32.45
+//var myFirstProgrammingLanguage:String = "JAVASCRIPT"
+//var bestProgrammingLanguage = myFirstProgrammingLanguage
+
+
+
+//Submission correctly answers Exercise 3.
+
+//var stringToReverse: String = "abcdefg"
+//let arrayOfCharacters = stringToReverse.characters.reversed()
+//stringToReverse = String(arrayOfCharacters)
+//print(stringToReverse)
+
+//Submission correctly answers Exercise 4.
+
+//var hungerLevel: Int = 5
+//var score: Int = 90
+//var studentBirthday: String = "2018/10/24"
+//var userTypes: String = "[a,b,c,d]"
+
+//Submission correctly answers Exercise 5.
+
+var word = "shampoo"
+if(word.contains("ham")){
+    print("this string contains the substring 'ham'")
+}
