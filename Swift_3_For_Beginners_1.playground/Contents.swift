d22f1e527af1544b3fe8b694478049c34a4bf71a
freedom-diy <170829561@qq.com>
2018-10-26 01:55:46 +0800

    add file

---
+++ Swift_3_For_Beginners_1.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Submission correctly answers Exercise 1.
+
+var petsAge = 12
+var myMiddleInitial: Character = "A"
+var centimetersOfRainfall: Float = 5.5
+var gallonsRemainingInTank: Double = 3.14159265359
+var ageOfSpecimenInWeeks: Int = 4
+var birthplace: String  = "nudibranch"
+var rainingOutside: Bool  = true
+
