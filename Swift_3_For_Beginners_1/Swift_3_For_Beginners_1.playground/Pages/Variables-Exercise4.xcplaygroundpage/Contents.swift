c6ca956bf8c888e906fb37a25776e5554cfd0569
freedom-diy <170829561@qq.com>
2018-11-17 01:42:28 +0800

    add exerciser 4 answer

---
+++ Swift_3_For_Beginners_1/Swift_3_For_Beginners_1.playground/Pages/Variables-Exercise4.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+import UIKit
+import Foundation
+
+//Exercise 4
+//
+//For the following descriptions, create a variable or constant that best represents it:
+//
+//an Int value representing the hunger level of an animal over time.
+//an Int value representing the score always required to win a game.
+//a String value representing a student’s birthday.
+//a String value representing what a user types into the status update area.
+
+var hungerOfAnimal:Int = 3
+var scoreOfWin:Int = 90
+var birthday:String = "2018/10/10"
+var status:String = "success"
+
+
+//: [Next](@next)
