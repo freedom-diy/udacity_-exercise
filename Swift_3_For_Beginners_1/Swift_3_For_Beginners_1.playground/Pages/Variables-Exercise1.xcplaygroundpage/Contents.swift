7b5aa382119ca93b10fc0cc6ddc98319159c7f87
freedom-diy <170829561@qq.com>
2018-11-17 00:06:41 +0800

    add exerciser 1 answer

---
+++ Swift_3_For_Beginners_1/Swift_3_For_Beginners_1.playground/Pages/Variables-Exercise1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+//Variables
+//Exercise 1
+//
+//For each of the following names and types, create a variable or constant that best represents it:
+//
+//gallonsRemainingInTank (Double)
+//ageOfSpecimenInWeeks (Int)
+//birthplace (String)
+//myFirstProgrammingLanguage (String)
+
+import UIKit
+import Foundation
+
+var gallonsRemainingInTank: Double = 3.1415926
+var ageOfSpecimenInWeeks: Int = 4
+var birthplace: String  = "China"
+var myFirstProgrammingLanguage: String  = "HTML"
+
+//: [Next](@next)
