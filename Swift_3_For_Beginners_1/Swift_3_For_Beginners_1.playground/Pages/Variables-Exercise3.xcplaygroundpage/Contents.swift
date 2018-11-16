d222bd24df818b8a0f04fa7a94e940057d010fe2
freedom-diy <170829561@qq.com>
2018-11-17 01:38:18 +0800

    add exerciser 3 answer

---
+++ Swift_3_For_Beginners_1/Swift_3_For_Beginners_1.playground/Pages/Variables-Exercise3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import UIKit
+import Foundation
+
+//Exercise 3
+//
+//Below is some code to reverse a string. We've declared stringToReverse and arrayOfCharacters as variables. Rewrite this code to reverse a string using only constants declared with let, no variables. Note: In the new version you may use as many constants as you'd like.
+
+//var stringToReverse = "Mutable or Immutable? That is the question."
+//var arrayOfCharacters = stringToReverse.characters.reversed()
+//stringToReverse = String(arrayOfCharacters)
+
+let haveATry: String = "dream"
+let arrayOfString = haveATry.reversed()
+let stringToReverse = String(arrayOfString)  //swft 4
+print(stringToReverse)
+
+//: [Next](@next)
