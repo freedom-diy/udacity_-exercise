445db4b7a820b1551ebe7c3dd216c9819dd0d4e3
freedom-diy <170829561@qq.com>
2018-11-17 00:22:14 +0800

    add exerciser 2 answer

---
+++ Swift_3_For_Beginners_1/Swift_3_For_Beginners_1.playground/Pages/Variables-Exercise2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+import UIKit
+import Foundation
+
+//Exercise 2
+//
+//Make the minimal changes necessary to fix the following compiler errors related to assignment, variables, and constants.
+//
+//var favoriteLanguage: Character = "Swift"
+//gallonsRemainingInTank = "2"
+//ageOfSpecimenInWeeks = 32.45
+//let bestProgrammingLanguage: Character = myFirstProgrammingLanguage
+
+var favoriteLanguage: String = "Swift"
+var gallonsRemainingInTank: Int = 2
+var ageOfSpecimenInWeeks: Float = 32.45
+var myFirstProgrammingLanguage: String = "HTML"
+let bestProgrammingLanguage: String = myFirstProgrammingLanguage
+
+//: [Next](@next)
