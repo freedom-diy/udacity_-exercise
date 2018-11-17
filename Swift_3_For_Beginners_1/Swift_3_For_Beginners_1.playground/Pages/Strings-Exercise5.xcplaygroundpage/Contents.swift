c3e0109e9b7d60e857a32049580ca9f8af3866df
freedom-diy <170829561@qq.com>
2018-11-17 17:51:11 +0800

    add exerciser 5 answer

---
+++ Swift_3_For_Beginners_1/Swift_3_For_Beginners_1.playground/Pages/Strings-Exercise5.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import UIKit
+import Foundation
+
+//Strings
+//
+//Exercise 5
+//
+//Write code that tells you whether or not this string contains the substring "ham".
+//
+//var word = "shampoo"
+
+var word = "shampoo"
+var isContains =  word.contains("ham")
+
+if isContains {
+    print("\(word) is contains the substring \"ham\". ")
+} else {
+    print("\(word) isn't contains the substring \"ham\". ")
+}
+
+//: [Next](@next)
